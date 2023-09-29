0476308805bd4acf3a854e1071b24e7da4b4de58
Adriana <acronmar1@gmail.com>
2023-09-29 14:04:33 +0300

    Triangle constructor rectified

--- src/main/java/ro/scoalainformala/shapes/ProgramShapes.java
+++ src/main/java/ro/scoalainformala/shapes/ProgramShapes.java
@@ -8,12 +8,17 @@ public class ProgramShapes {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-
+        System.out.print("Please enter the first length of the tiangle: ");
         double x = s.nextDouble();
+        System.out.print("Please enter the second length of the tiangle: ");
         double y = s.nextDouble();
+        System.out.print("Please enter the third length of the tiangle: ");
         double z = s.nextDouble();
 
-        new Triangle(x, y, z);
+        Triangle t = new Triangle(x, y, z);
+        System.out.println("\nThe tiangle area is: " + t.calculateArea());
+
+        System.out.println("The tiangle perimeter is: " + t.calculatePerimeter());
 
 
     }
--- src/main/java/ro/scoalainformala/shapes/domain/Triangle.java
+++ src/main/java/ro/scoalainformala/shapes/domain/Triangle.java
@@ -3,7 +3,7 @@ package ro.scoalainformala.shapes.domain;
 public class Triangle {
     private double l1, l2, l3;
 
-    public Tiangle(double l1, double l2, double l3) {
+    public Triangle(double l1, double l2, double l3) {
         this.l1 = l1;
         this.l2 = l2;
         this.l3 = l3;
